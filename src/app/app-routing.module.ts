ad4058aa089d48a4a5d0564ed3ea06e21f3e67c6
buihieuhoan <buihieuhoan@gmail.com>
2024-02-23 15:19:59 +0700

    6_Add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  //add the dashboard route
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  //add a default route
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  //add a hero detail route
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ]
 })
+
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  template: `
+    <h1> {{title}} </h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+    <app-messages></app-messages>
+  `
 })
 
 export class AppComponent {
