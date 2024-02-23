5e5d6a72ab945679210c703c7acba71253a7ddec
buihieuhoan <buihieuhoan@gmail.com>
2024-02-23 10:25:28 +0700

    5_Add services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { HeroesComponent } from "./heroes/heroes.component";
-import { AppComponent } from "./app.component";
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
 
-@NgModule ({
-    declarations: [
-        AppComponent,
-        HeroesComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        FormsModule,
-        BrowserModule,
-        HeroesComponent
-    ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+
+  providers: [],
+  
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,20 +1,12 @@
-import {Component, Input} from '@angular/core';
-import {NgIf, UpperCasePipe} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {Hero} from '../hero';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
-  standalone: true,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
-  imports: [
-    FormsModule, 
-    NgIf, 
-    UpperCasePipe
-  ],
+  styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent {
-  @Input() hero?: Hero;
+  @Input() hero: Hero | undefined;
 }
